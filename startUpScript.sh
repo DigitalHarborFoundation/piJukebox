6600388e77949db6c62fc1e5c16cd2059bb83dca
Shawn Grimes <shawn@campfireapps.com>
2014-01-14 22:51:51 -0500

    -added simple startup script

---
+++ startUpScript.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+PIJUKEBOX_HOME=/home/pi/piJukebox
+
+# start mopidy
+/usr/local/bin/mopidy --config $PIJUKEBOX_HOME/.config/mopidy/mopidy.conf
+
+#give mopidy 60 seconds to start up
+sleep 60
+
+# start the jukeboxUI
+/usr/bin/python PIJUKEBOX_HOME/piJukebox/piJukebox.py
